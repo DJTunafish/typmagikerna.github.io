f03157d6b8b1ce3822a246bdf975f1f0db7183a5
Daniel Beecham <daniel@lunix.se>
2016-07-28 19:04:16 +0000

    improve navscroll.js formatting

--- js/navscroll.js
+++ js/navscroll.js
@@ -1,13 +1,19 @@
-window.onscroll = (function() {
-  var isOnTop = true,
-    main_nav = $("#main-nav");
-  return function() {
-    if (document.body.scrollTop < 50 && isOnTop === false) {
-      main_nav.animate({backgroundColor: "rgba(0,0,0,0)"}, 250);
-      isOnTop = true;
-    } else if (document.body.scrollTop > 50 && isOnTop === true) {
-      main_nav.animate({backgroundColor: "black"}, 250);
-      isOnTop = false;
-    }
-  };
+/*global
+  window, $, document
+*/
+
+window.onscroll = (function () {
+    'use strict';
+
+    var isOnTop = true,
+        main_nav = $("#main-nav");
+    return function () {
+        if (document.body.scrollTop < 50 && isOnTop === false) {
+            main_nav.animate({backgroundColor: "rgba(0,0,0,0)"}, 250);
+            isOnTop = true;
+        } else if (document.body.scrollTop > 50 && isOnTop === true) {
+            main_nav.animate({backgroundColor: "black"}, 250);
+            isOnTop = false;
+        }
+    };
 }());
