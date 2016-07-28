4cd1b5871996490022dfba16f2c8cf382beb67db
Daniel Beecham <daniel@lunix.se>
2016-07-28 19:19:43 +0000

    Changed nav colors to match background.

--- js/navscroll.js
+++ js/navscroll.js
@@ -2,6 +2,9 @@
   window, $, document
 */
 
+(function () {
+}());
+
 window.onscroll = (function () {
     'use strict';
 
@@ -12,7 +15,7 @@ window.onscroll = (function () {
             main_nav.animate({backgroundColor: "rgba(0,0,0,0)"}, 250);
             isOnTop = true;
         } else if (document.body.scrollTop > 50 && isOnTop === true) {
-            main_nav.animate({backgroundColor: "black"}, 250);
+            main_nav.animate({backgroundColor: "#404040"}, 250);
             isOnTop = false;
         }
     };
